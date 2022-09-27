2ee6a4ebbc6edd70f5579a416774e4b18cfd06f8
JoaoAfranio <joaoafraniorosa@hotmail.com>
2022-09-27 16:51:05 -0300

    initial layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,29 @@
+import logo from "../assets/img/logo.png"
+
 export default function App(){
-    return <h1>Hello World!</h1>;
+    return (
+        <div class="home">
+            <div class="deck">
+                <div class="header">
+                    <img src={logo} alt="logo"/>
+                    Zaprecall
+                </div>
+                <div class="main">
+                    <div class="flashcard"> </div>
+                    <div class="flashcard"> </div>
+                    <div class="flashcard"> </div>
+                    <div class="flashcard"> </div>
+                </div>
+                <div class="footer">
+                    <div class="action">
+                        <span class="button error">dsads</span>
+                        <span class="button almost">dsads</span>
+                        <span class="button zap">dsads</span>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    );
 }
