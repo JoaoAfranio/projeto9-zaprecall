5f97959ac4dbc96394a091384cc8b104b8a268e6
JoaoAfranio <joaoafraniorosa@hotmail.com>
2022-09-29 18:32:57 -0300

    fixing colors svg

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,168 +1,186 @@
-import logo from "../assets/img/logo.png"
+import logo from "../assets/img/logo.png";
 import React from "react";
 import Flashcard from "./Flashcard";
 import styled from "styled-components";
 
-import checkmark from "../assets/img/checkmark.svg"
-import close from "../assets/img/close.svg"
-import help from "../assets/img/help.svg"
-
-
-
+import { ReactComponent as Checkmark } from "../assets/img/checkmark.svg";
+import { ReactComponent as Close } from "../assets/img/close.svg";
+import { ReactComponent as Help } from "../assets/img/help.svg";
 
 const flashcards = [
-    {pergunta: "O que é JSX?", resposta: "Uma extensão de linguagem do JavaScript"},
-    {pergunta: "O React é __ ", resposta: "Uma biblioteca JavaScript para construção de interfaces"},
-    {pergunta: "Componentes devem iniciar com __", resposta: "Letra maiúscula"},
-    {pergunta: "Podemos colocar __ dentro do JSX", resposta: "Expressões"},
+  {
+    pergunta: "O que é JSX?",
+    resposta: "Uma extensão de linguagem do JavaScript",
+  },
+  {
+    pergunta: "O React é __ ",
+    resposta: "Uma biblioteca JavaScript para construção de interfaces",
+  },
+  { pergunta: "Componentes devem iniciar com __", resposta: "Letra maiúscula" },
+  { pergunta: "Podemos colocar __ dentro do JSX", resposta: "Expressões" },
 ];
 
+export default function Deck() {
+  const [focusCard, setFocusCard] = React.useState();
+  const [showAnswerFocusCard, setShowAnswerFocusCard] = React.useState(false);
+  const [listAnswerCards, setlistAnswerCards] = React.useState([]);
 
-export default function Deck(){
+  const [queueAnswers, setQueueAnswers] = React.useState([]);
+  const [hasAnswerAllCards, setHasAnswerAllCards] = React.useState(false);
 
-    const [focusCard, setFocusCard] = React.useState();
-    const [showAnswerFocusCard, setShowAnswerFocusCard] = React.useState(false);
-    const [listAnswerCards, setlistAnswerCards] = React.useState([]);
+  const checkMarkSVG = <Checkmark className="acerto" alt="acerto" />;
+  const helpSVG = <Help className="help" alt="help" />;
+  const closeSVG = <Close className="erro" alt="erro" />;
 
-    const [queueAnswers, setQueueAnswers] = React.useState([]);
-    const [hasAnswerAllCards, setHasAnswerAllCards] = React.useState(false);
+  function openSelectedCard(i) {
+    setFocusCard(i);
+    setShowAnswerFocusCard(false);
+  }
 
-    function openSelectedCard(i) {
-        setFocusCard(i)
-        setShowAnswerFocusCard(false);
+  function showAnswerCard() {
+    setShowAnswerFocusCard(true);
+  }
+
+  function getAnswerCard(idx) {
+    const hasAnswer = listAnswerCards.find((a) => a.id === idx);
+    if (hasAnswer) {
+      return { isAnswered: true, typeOfAnswer: hasAnswer.answer };
+    } else {
+      return { isAnswered: false, typeOfAnswer: "" };
     }
+  }
 
-    function showAnswerCard() {
-        setShowAnswerFocusCard(true)
-    }
-    
-    function getAnswerCard(idx) {
-        const hasAnswer = listAnswerCards.find((a) => a.id === idx);
-        if(hasAnswer) {
-            return {isAnswered: true, typeOfAnswer: hasAnswer.answer}
-        } else {
-            return {isAnswered: false, typeOfAnswer: ""}
-        }
-    }
+  function answerCard(ans) {
+    if (focusCard === undefined) return;
 
-    function answerCard(ans) {
-        if(focusCard === undefined) return;
+    const newList = [...listAnswerCards, { id: focusCard, answer: ans }];
+    setlistAnswerCards(newList);
+    setFocusCard();
 
-        const newList = [...listAnswerCards, {id: focusCard, answer: ans}]
-        setlistAnswerCards(newList)
-        setFocusCard();
-        
-        const lengthLista = listAnswerCards.length + 1;
-        if(lengthLista === flashcards.length) AnsweredAllCards(newList);
-    }
+    const lengthLista = listAnswerCards.length + 1;
+    if (lengthLista === flashcards.length) AnsweredAllCards(newList);
+  }
 
-    function AnsweredAllCards(list) {
-        const ordenedList = list.sort((a,b) => a.id > b.id)
-        const layoutQueue = ordenedList.map((ans) => { 
-            return (
-                <div key= {ans.id} class="icone">
-                    <img className={ans.answer} src={
-                    ans.answer === "acerto" ? checkmark : ans.answer === "erro" ? close : help} 
-                    alt={ans.answer}></img>
-                </div>
-            )
-        })
+  function AnsweredAllCards(list) {
+    const ordenedList = list.sort((a, b) => a.id > b.id);
+    const layoutQueue = ordenedList.map((ans) => {
+      return (
+        <div key={ans.id} class="icone">
+          {ans.answer === "acerto"
+            ? checkMarkSVG
+            : ans.answer === "erro"
+            ? closeSVG
+            : helpSVG}
+        </div>
+      );
+    });
 
-        setHasAnswerAllCards(true);
-        setQueueAnswers(layoutQueue);
-    }
+    setHasAnswerAllCards(true);
+    setQueueAnswers(layoutQueue);
+  }
 
-
-    return (
-        <Container>
-            <Header>
-                <img src={logo} alt="logo"/>
-                <h1>ZapRecall</h1>
-            </Header>
-            <Main>
-
-                {flashcards.map((f, idx) => {
-                    return <Flashcard 
-                                id={idx}
-                                content={f} 
-                                isFocusCard={focusCard === idx ? true : false}
-                                showAnswerFocusCard={showAnswerFocusCard}
-                                showAnswerCard={showAnswerCard}
-                                openSelectedCard={openSelectedCard}
-                                key={idx}
-                                answer={getAnswerCard(idx)}
-                                answerCard={answerCard}
-                                />
-                })}
-
-            </Main>
-            <Footer>
-                <p>{listAnswerCards.length}/{flashcards.length} CONCLUÍDOS</p>
-                <Queue>{hasAnswerAllCards === true ? queueAnswers : ""}</Queue>
-                
-            </Footer>
-        </Container>
-    );
+  return (
+    <Container>
+      <Header>
+        <img src={logo} alt="logo" />
+        <h1>ZapRecall</h1>
+      </Header>
+      <Main>
+        {flashcards.map((f, idx) => {
+          return (
+            <Flashcard
+              id={idx}
+              content={f}
+              isFocusCard={focusCard === idx ? true : false}
+              showAnswerFocusCard={showAnswerFocusCard}
+              showAnswerCard={showAnswerCard}
+              openSelectedCard={openSelectedCard}
+              key={idx}
+              answer={getAnswerCard(idx)}
+              answerCard={answerCard}
+            />
+          );
+        })}
+      </Main>
+      <Footer>
+        <p>
+          {listAnswerCards.length}/{flashcards.length} CONCLUÍDOS
+        </p>
+        <Queue>{hasAnswerAllCards === true ? queueAnswers : ""}</Queue>
+      </Footer>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Main = styled.div`
-    max-height: 400px;
-    overflow-y: auto;
+  max-height: 400px;
+  overflow-y: auto;
 
-    &::-webkit-scrollbar {
-        display: none;
-    }
-`
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
 
 const Header = styled.div`
-    position: fixed;
-    height: 80px;
-    top: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 10px 0;
-    font-size: 36px;
-    color: white;
-    font-family: "Righteous";
-    z-index: 1;
-    background-color: var(--cor-fundo);
+  position: fixed;
+  height: 80px;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 0;
+  font-size: 36px;
+  color: white;
+  font-family: "Righteous";
+  z-index: 1;
+  background-color: var(--cor-fundo);
 
-    img {
-        width: 52px;
-        height: 60px;
-    }
-`
+  img {
+    width: 52px;
+    height: 60px;
+  }
+`;
 
 const Footer = styled.div`
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: white;
-    z-index: 1;
-    width: 100%;
-    min-height: 70px;
-    padding: 14px 10px;
-    font-size: 18px;
-    color: black;
-    position: fixed;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: white;
+  z-index: 1;
+  width: 100%;
+  min-height: 70px;
+  padding: 14px 10px;
+  font-size: 18px;
+  color: black;
+  position: fixed;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
 
 const Queue = styled.div`
-    display: flex;
-    justify-content: center;
-    gap: 25px;
-`
+  display: flex;
+  justify-content: center;
+  gap: 25px;
 
+  svg {
+    &.acerto {
+      fill: var(--cor-zap);
+    }
+    &.help {
+      fill: var(--cor-quase-nao-lembrei);
+    }
+    &.erro {
+      fill: var(--cor-nao-lembrei);
+    }
+  }
+`;
--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -1,108 +1,135 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-import playOutline from "../assets/img/play-outline-icon.svg"
-import setinha from "../assets/img/setinha.png"
+import setinha from "../assets/img/setinha.png";
 
-import checkmark from "../assets/img/checkmark.svg"
-import close from "../assets/img/close.svg"
-import help from "../assets/img/help.svg"
+import Action from "./Action";
 
-import Action from "./Action"
+import { ReactComponent as PlayOutline } from "../assets/img/play-outline-icon.svg";
+import { ReactComponent as Checkmark } from "../assets/img/checkmark.svg";
+import { ReactComponent as Close } from "../assets/img/close.svg";
+import { ReactComponent as Help } from "../assets/img/help.svg";
 
+export default function Flashcard({
+  id,
+  content,
+  isFocusCard,
+  showAnswerFocusCard,
+  showAnswerCard,
+  openSelectedCard,
+  answer,
+  answerCard,
+}) {
+  const layoutResposta = [content.resposta, <Action answerCard={answerCard} />];
+  const layoutPergunta = [
+    content.pergunta,
+    <div>
+      {" "}
+      <img
+        src={setinha}
+        alt="play"
+        onClick={() => {
+          showAnswerCard();
+        }}
+      ></img>
+    </div>,
+  ];
 
+  const checkMarkSVG = <Checkmark className="acerto" alt="acerto" />;
+  const helpSVG = <Help className="help" alt="help" />;
+  const closeSVG = <Close className="erro" alt="erro" />;
 
-export default function Flashcard({id, content, isFocusCard, showAnswerFocusCard, showAnswerCard, openSelectedCard, answer, answerCard}) {
-    const layoutResposta = [content.resposta, <Action answerCard={answerCard}/>]
-    const layoutPergunta = [content.pergunta,<div> <img src={setinha} alt="play" onClick={() => {showAnswerCard()}}></img></div>]
-
-    function renderFlashcard() {
-        if(isFocusCard === true){
-            return (
-                <Content className="aberto"> 
-                        {showAnswerFocusCard === true ? layoutResposta : layoutPergunta}
-
-                </Content>
-            )
-        } else if(answer.isAnswered === true) {
-            return (
-                <Content className={answer.typeOfAnswer}> 
-                    Pergunta {id + 1}
-                    <div class="icone">
-                        <img className={answer.typeOfAnswer} src={
-                            answer.typeOfAnswer === "acerto" ? checkmark : answer.typeOfAnswer === "erro" ? close : help} 
-                        alt={answer.typeOfAnswer}></img>
-                    </div>
-
-                </Content>
-            )
-        } 
-        else {
-            return (
-                <Content onClick={() => {openSelectedCard(id)}}> 
-                    Pergunta {id + 1}
-                    <img className="icone" src={playOutline} alt="play"></img>
-                </Content>
-            )
-        }
+  function renderFlashcard() {
+    if (isFocusCard === true) {
+      return (
+        <Content className="aberto">
+          {showAnswerFocusCard === true ? layoutResposta : layoutPergunta}
+        </Content>
+      );
+    } else if (answer.isAnswered === true) {
+      return (
+        <Content className={answer.typeOfAnswer}>
+          Pergunta {id + 1}
+          <div class="icone">
+            {answer.typeOfAnswer === "acerto"
+              ? checkMarkSVG
+              : answer.typeOfAnswer === "erro"
+              ? closeSVG
+              : helpSVG}
+          </div>
+        </Content>
+      );
+    } else {
+      return (
+        <Content
+          onClick={() => {
+            openSelectedCard(id);
+          }}
+        >
+          Pergunta {id + 1}
+          <PlayOutline className="icone" />
+        </Content>
+      );
     }
+  }
 
-    return (
-        renderFlashcard()
-    )
+  return renderFlashcard();
 }
 
 const Content = styled.div`
-    width: 300px;
-    height: 50px;
+  width: 300px;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  color: black;
+  border-radius: 5px;
+  margin: 10px 0px;
+  padding: 10px 10px;
+  cursor: pointer;
+  font-family: "Recursive", cursive;
+
+  &.aberto {
+    min-height: 130px;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: white;
-    color: black;
-    border-radius: 5px;
-    margin: 10px 0px;
-    padding: 10px 10px;
-    cursor: pointer;
-    font-family: 'Recursive', cursive;
+    flex-direction: column;
+    background-color: var(--cor-fundo-card);
+    cursor: default;
+    padding: 20px 10px;
 
-    &.aberto {
-        min-height: 130px;
-        display: flex;
-        flex-direction: column;
-        background-color: var(--cor-fundo-card);
-        cursor: default;
-        padding: 20px 10px;
-
-        &.resposta div{
-            width: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-
-        div {
-            width: 100%;
-            display: flex;
-            justify-content: flex-end;
-        }
-
-        img {
-            cursor: pointer;
-        }
+    &.resposta div {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
 
-    &.acerto {
-        color: var(--cor-zap);
-        text-decoration: line-through;
+    div {
+      width: 100%;
+      display: flex;
+      justify-content: flex-end;
     }
 
-    &.help {
-        color: var(--cor-quase-nao-lembrei);
-        text-decoration: line-through;
+    img {
+      cursor: pointer;
     }
+  }
 
-    &.erro {
-        color: var(--cor-nao-lembrei);
-        text-decoration: line-through;  
-    }
+  &.acerto {
+    color: var(--cor-zap);
+    fill: var(--cor-zap);
+    text-decoration: line-through;
+  }
+
+  &.help {
+    color: var(--cor-quase-nao-lembrei);
+    fill: var(--cor-quase-nao-lembrei);
+    text-decoration: line-through;
+  }
+
+  &.erro {
+    color: var(--cor-nao-lembrei);
+    fill: var(--cor-nao-lembrei);
+    text-decoration: line-through;
+  }
 `;
